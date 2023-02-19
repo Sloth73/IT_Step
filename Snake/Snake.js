8ebb954dbb0974a9b82dd82bb732ae0e205f3c33
Sloth73 <Pichl.tomas.95@gmail.com>
2023-02-19 20:29:58 +0100

    Added request animation frame function - rendering canvas with snake

--- Snake/Snake.js
+++ Snake/Snake.js
@@ -2,14 +2,19 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
 //Snake style
 const snakeSize = 50;
 let snakePositionX = 0;
 let snakePositionY = canvas.height/2 - snakeSize/2
 
-ctx.fillStyle = 'black';
-ctx.fillRect(snakePositionX, snakePositionY, snakeSize, snakeSize);
+function gameCycle() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(snakePositionX, snakePositionY, snakeSize, snakeSize);
+
+    requestAnimationFrame(gameCycle);
+}
+
+gameCycle()
