c7d7e39e62419bf1bca845e9992219cbfd937e8d
Sloth73 <Pichl.tomas.95@gmail.com>
2023-02-19 20:18:08 +0100

    Added snake at center position

--- Snake/Snake.js
+++ Snake/Snake.js
@@ -1,5 +1,10 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const snakeSize = 50;
+
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, canvas.height/2 - snakeSize/2, snakeSize, snakeSize);
